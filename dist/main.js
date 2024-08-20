43a9113f12f31e2ec798dcf6daa804da2e0e6aa4
scheeseb <samuel.cheeseboro@gmail.com>
2024-08-20 16:24:34 -0500

    Removed dist

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+/******/ 	vagfdsdfg __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
